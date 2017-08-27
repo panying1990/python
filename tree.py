10e9fcad3198e1e97d9e897a6610e9e91397d1d4
panying1990 <panying1298247841@163.com>
2017-08-27 23:04:08 +0800

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,5 +1,15 @@
-'''计算给定数据集的香农熵：即数据集数据不一致性的程度'''
+'''决策树ID3算法环境'''
+from numpy import *
+import operator
 from math import log   '''引用对数函数'''
+
+''' 用于创建用于分析的数据集'''
+def createDataSet()
+    group = arrary([1.0,1.1],[1.0,1.0],[0,0],[0,0.1])
+    label = ["A","A","B","B"] 
+    return group, labels
+
+'''计算给定数据集的香农熵：即数据集数据不一致性的程度'''
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
@@ -15,9 +25,18 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -=prob * log(prob,2)
     return ShannonEnt 
- 
+
+'''按照给定特征划分数据集''' 
 def splitDataSet(dataSet, axis, value):
-    retDataSet = []
+    retDataSet = []  '''创建新的list对象'''
     for featVec in dataSet:
+        '''抽取符合标准的值'''
         if featVec[axis]  == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extned(featVec[axis+1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+'''选择最好的数据集划分方式'''
+def chooseBestFeatureTo
             
