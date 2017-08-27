93a8d4b3028e3aa1665d1e42d18f1470dbf93d81
panying1990 <panying1298247841@163.com>
2017-08-27 22:45:32 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,17 @@
+'''计算给定数据集的香农熵：即数据集数据不一致性的程度'''
+from math import log   '''引用对数函数'''
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    '''为所有可能分类创建字典'''
+    for featVec in dataSet:
+        currentLabel = featCec[-1]
+        if currentLabel not in labelCounts.keys():
+           labelCounts[curreLabel] = 0
+        labelCounts[currentLabel] += 1
+    '''参照信息熵公式，以2为底求对数'''
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -=prob * log(prob,2)
+    return shannonEnt
