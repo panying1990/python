8c9d33f25544abfb68448a159dbfba50d6ce9d3c
panying <panying1298247841@163.com>
2018-01-11 23:37:43 +0800

    Update selfhelp_input.py

--- selfhelp_input.py
+++ selfhelp_input.py
@@ -21,7 +21,7 @@ def import_date():
     dt = datetime.datetime.now ()
     return dt.strftime("%Y-%m-%d")
 
-# 获得符合条件的自助取数对象（file_name =[碧生源官方旗舰店,碧生源药品旗舰店,[1-9]]）
+# 获得符合条件的自助取数对象（file_name =[碧生源官方旗舰店,碧生源药品旗舰店]）
 def get_shopfile(SourceDir,file_name):
     SourceDirlist = os.listdir (SourceDir)
     os.chdir (SourceDir)
@@ -40,7 +40,31 @@ def get_shopfile(SourceDir,file_name):
             continue
     return filelist,file_name
 
-# 对目标文件进处理并合并(流量、交易、服务、其他)
+# 获得符合条件的自助取数对象（file_name =商品名）
+def get_productfile(SourceDir):
+    os.chdir (SourceDir)
+    try:
+        temp_path = os.getcwd ()
+        if temp_path == SourceDir:
+            print ("The work file path was change!")
+    except:
+        print ("The work file path havn't change!")
+    # 获得目标文件夹中的目标文件
+    filelist = os.listdir (SourceDir)
+    product_file = []
+    for filename in filelist:
+        if ('4' in filename) or ('5' in filename):
+            product_file.append (filename)
+    product_name = []
+    for i in range ((len (product_file))):
+        temp_name = product_file[i].split ('_')[0]
+        if temp_name in product_name:
+            continue
+        else:
+            product_name.append (temp_name)
+    return filelist, product_name
+
+# 对目标未店铺的数据进处理并合并(流量、交易、服务、其他)
 def Merge_file(filelist,file_name,dt):
     file_num = len (filelist)
     matrix = [None] * file_num
@@ -84,7 +108,62 @@ def Merge_file(filelist,file_name,dt):
     dataset["添加时间"] = dt
     return dataset
 
-# 将目标数据集插入到Mysql目标数据集合
+# 对目标未店铺的数据进处理并合并(流量、交易、服务、其他)
+def Merge_productfile(filelist, product_name,dt):
+    temp = ['统计日期', 't_PC端支付金额(元)', 't_PC端支付买家数', 't_无线端支付金额(元)', 't_无线端支付买家数',
+            't_下单转化率', 't_支付转化率', 't_支付金额(元)', '商品名称', 'o_加购人数', 'o_加购商品件数',
+            'o_PC端加购商品件数', 'o_商品收藏人数', 'o_无线端加购商品件数', 'f_访客数', 'f_浏览量', 'f_PC端访客数',
+            'f_无线端访客数', '商品ID', '添加时间']
+    dataset = pd.DataFrame (columns=temp)
+    for filename in product_name:
+        product = []
+        for i in range (len (filelist)):
+            if filename in filelist[i]:
+                product.append (filelist[i])
+            else:
+                continue
+        matrix = [None] * len (product)
+        matrix_name = [None] * len (product)
+        for j in range (len (product)):
+            table = xlrd.open_workbook (product[j])
+            sheet = table.sheet_by_name (u'自助取数')
+            product_title = sheet.cell (3, 0).value
+            colname = sheet.row_values (4, )
+            matrix_name[j] = []
+            matrix_name[j].append (colname[0])
+            if "交易" in product[j]:
+                for c in range (1, len (colname)):
+                    matrix_name[j].append ("t_" + colname[c])
+            elif "流量" in product[j]:
+                for c in range (1, len (colname)):
+                    matrix_name[j].append ("f_" + colname[c])
+            else:
+                for c in range (1, len (colname)):
+                    matrix_name[j].append ("o_" + colname[c])
+            matrix_name[j].append ("商品名称")
+            nrows = sheet.nrows - 5
+            matrix[j] = [0] * (nrows)
+            ncols = sheet.ncols
+            for m in range (nrows):
+                matrix[j][m] = ["0"] * (ncols + 1)
+            for k in range (nrows):
+                for d in range (ncols + 1):
+                    if d < ncols:
+                        matrix[j][k][d] = sheet.cell (k + 5, d).value
+                    else:
+                        matrix[j][k][d] = product_titlet
+        data1 = pd.DataFrame (data=matrix[0], columns=matrix_name[0])
+        data2 = pd.DataFrame (data=matrix[1], columns=matrix_name[1])
+        data3 = pd.DataFrame (data=matrix[2], columns=matrix_name[2])
+        data4 = pd.merge (data1, data2, on=['统计日期', '商品名称'])
+        data5 = pd.merge (data4, data3, on=['统计日期', '商品名称'])
+        data5["商品ID"] = filename
+        data5["添加时间"] = dt
+        temp = dataset
+        dataset = pd.concat ([temp, data5])
+    return dataset
+
+# 将店铺目标数据集插入到Mysql目标数据集合
 def exportmysql(dataset):
     # 连接上数据库
     connection = pymysql.connect (host='192.168.111.251',
