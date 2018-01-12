afb51ba49abc7e047fbc7357d1ff138d413f909d
panying <panying1298247841@163.com>
2018-01-12 14:17:27 +0800

    Update selfhelp_input.py

--- selfhelp_input.py
+++ selfhelp_input.py
@@ -4,7 +4,7 @@
  格式转化：将从WEB的EXCEL2003格式数据存储到MySql中；
  存储对象：1、店铺；2、商品两个对象
  数据模块：流量=f、交易=t、服务=s、其他=o四个模块；
- 最近编辑时间：2018-01-10
+ 最近编辑时间：2018-01-12
 """
 __author__ = 'Pan Ying'
 __email__ = 'panying1298247841@163.com'
@@ -108,7 +108,7 @@ def Merge_file(filelist,file_name,dt):
     dataset["添加时间"] = dt
     return dataset
 
-# 对目标未店铺的数据进处理并合并(流量、交易、服务、其他)
+# 对目标商品的数据进处理并合并(流量、交易、服务、其他)
 def Merge_productfile(filelist, product_name,dt):
     temp = ['统计日期', 't_PC端支付金额(元)', 't_PC端支付买家数', 't_无线端支付金额(元)', 't_无线端支付买家数',
             't_下单转化率', 't_支付转化率', 't_支付金额(元)', '商品名称', 'o_加购人数', 'o_加购商品件数',
@@ -151,7 +151,7 @@ def Merge_productfile(filelist, product_name,dt):
                     if d < ncols:
                         matrix[j][k][d] = sheet.cell (k + 5, d).value
                     else:
-                        matrix[j][k][d] = product_titlet
+                        matrix[j][k][d] = product_title
         data1 = pd.DataFrame (data=matrix[0], columns=matrix_name[0])
         data2 = pd.DataFrame (data=matrix[1], columns=matrix_name[1])
         data3 = pd.DataFrame (data=matrix[2], columns=matrix_name[2])
@@ -172,26 +172,48 @@ def exportmysql(dataset):
                                   db='master_data',
                                   charset='utf8',
                                   cursorclass=pymysql.cursors.DictCursor)
-    try:
-        with connection.cursor () as cursor:
-                sql_rd = 'INSERT INTO `selfhelp_shop_data`(`统计日期`,`t_客单价`,`t_PC端客单价`,`t_无线端客单价`,' \
-                         '`t_老买家数`,`t_新买家数`,`t_下单买家数`,`t_下单商品件数`,`t_支付买家数`,`t_支付商品件数`,' \
-                         '`t_下单且支付金额`,`o_店铺收藏次数`,`o_店铺收藏人数`,`o_商品收藏次数`,`o_商品收藏人数`,`s_售中申请退款金额`,' \
-                         '`s_售中申请退款买家数`,`f_访客数`,`f_老访客数`,`f_PC端访客数`,`f_无线端访客数`,`店铺名称`,`添加时间`) ' \
-                         'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    if list(product_dataset.shape)[1] == 20:
+        try:
+            with connection.cursor () as cursor:
+                sql_rd = 'INSERT INTO `selfhelp_product_data`(`统计日期`, `t_PC端支付金额`, `t_PC端支付买家数`, `t_无线端支付金额`,'\
+                        '`t_无线端支付买家数`,`t_下单转化率`, `t_支付转化率`, `t_支付金额`, `商品名称`, `o_加购人数`, `o_加购商品件数`,'\
+                        '`o_PC端加购商品件数`, `o_商品收藏人数`, `o_无线端加购商品件数`, `f_访客数`, `f_浏览量`, `f_PC端访客数`,'\
+                        '`f_无线端访客数`, `商品ID`, `添加时间`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
+                          %s, %s, %s, %s, %s)'
                 for i in range (len (dataset)):
-                    valuetuple = tuple (list (dataset.values[i, ]))
+                    valuetuple = tuple (list (dataset.values[i,]))
                     cursor.execute (sql_rd, valuetuple)
-        connection.commit()
+            connection.commit ()
 
-        with connection.cursor() as cursor:
-            # Read a single record
-            sql = "SELECT max(`统计日期`) as 最晚时间 FROM `selfhelp_shop_data`"
-            cursor.execute (sql)
-            result = cursor.fetchone ()
-            print (result)
-    finally:
-        connection.close ()
+            with connection.cursor () as cursor:
+                # Read a single record
+                sql = "SELECT max(`统计日期`) as 最晚时间 FROM `selfhelp_product_data`"
+                cursor.execute (sql)
+                result = cursor.fetchone ()
+                print (result)
+        finally:
+            connection.close ()
+    else:
+        try:
+            with connection.cursor () as cursor:
+                    sql_rd = 'INSERT INTO `selfhelp_shop_data`(`统计日期`,`t_客单价`,`t_PC端客单价`,`t_无线端客单价`,' \
+                             '`t_老买家数`,`t_新买家数`,`t_下单买家数`,`t_下单商品件数`,`t_支付买家数`,`t_支付商品件数`,' \
+                             '`t_下单且支付金额`,`o_店铺收藏次数`,`o_店铺收藏人数`,`o_商品收藏次数`,`o_商品收藏人数`,`s_售中申请退款金额`,' \
+                             '`s_售中申请退款买家数`,`f_访客数`,`f_老访客数`,`f_PC端访客数`,`f_无线端访客数`,`店铺名称`,`添加时间`) ' \
+                             'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                    for i in range (len (dataset)):
+                        valuetuple = tuple (list (dataset.values[i, ]))
+                        cursor.execute (sql_rd, valuetuple)
+            connection.commit()
+
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT max(`统计日期`) as 最晚时间 FROM `selfhelp_shop_data`"
+                cursor.execute (sql)
+                result = cursor.fetchone ()
+                print (result)
+        finally:
+            connection.close ()
     return print("成功插入")
 
 def getshopname():
@@ -201,10 +223,15 @@ def getshopname():
 # 主函数
 if __name__ == '__main__':
     SourceDir = "V:\\更新数据\\碧生源官方旗舰店\\自助取数\\取数-12.10"
-    file_name = getshopname()
     dt = import_date()
+    # 店铺数据更新
+    file_name = getshopname ()
     for name in file_name:
         filelist,filename = get_shopfile(SourceDir,name)
         dataset = Merge_file (filelist,name,dt)
         print(dataset.axes)
         exportmysql (dataset)
+    # 商品数据更新
+    filelist, product_name = get_productfile (SourceDir)
+    product_dataset = Merge_productfile(filelist, product_name,dt)
+    exportmysql(product_dataset)
