ffb1b586a84d05f996e9130e6d9bcf295e7c84c5
panying <panying1298247841@163.com>
2018-02-24 09:35:24 +0800

    Update 天猫店铺流量数据转化合并导入.py

--- 天猫店铺流量数据转化合并导入.py
+++ 天猫店铺流量数据转化合并导入.py
@@ -17,6 +17,7 @@ import xlrd
 import pandas as pd
 import datetime
 import pymysql.cursors
+import string
 
 
 # 时间设置
@@ -46,8 +47,8 @@ def get_flow_file(source_dir, flow_name):
     return file_list
 
 
-# 将得到的店铺流量来源文件按照要求进行转换并进行输出
-def tranfer_file(file_list, flow_name):
+# 将得到的店铺流量来源文件按照要求进行转换,清洗后,并将数据导入数据库
+def inport_data(file_list, flow_name):
     file_num = len (file_list)
     matrix = [None] * file_num
     matrix_name = []
@@ -71,11 +72,13 @@ def tranfer_file(file_list, flow_name):
             matrix[i][m] = ["0"] * cols_num
         for j in range (rows_num):
             for k in range (cols_num):
-                matrix[i][j][k] = sheet.cell (j + 6, k).value
+                 matrix[i][j][k] = sheet.cell (j + 6, k).value
         temp_dataframe = pd.DataFrame (data=matrix[i], columns=matrix_name)
         temp_dataframe["流量来源"] = flow_name
         temp_dataframe["流量日期"] = flow_date
         temp_dataframe["添加日期"] = datetime.datetime.now ().strftime ("%Y-%m-%d")
+        std_data = get_std_data(temp_dataframe)
+        export_mysql(std_data)
         if "PC" in flow_name:
             target_dir_file = "V:\\更新数据\\碧生源官方旗舰店\\店铺流量来源\\PC流量来源_转化\\" + flow_file_name[:-4] + ".xls"
             temp_dataframe.to_excel (target_dir_file, index=False)
@@ -86,7 +89,7 @@ def tranfer_file(file_list, flow_name):
 
 
 # 将店铺目标数据集插入到Mysql目标数据集合
-def export_mysql(dataset):
+def export_mysql(data_set):
     # 连接上数据库
     connection = pymysql.connect (host='192.168.111.251',
                                   user='root',
@@ -94,43 +97,48 @@ def export_mysql(dataset):
                                   db='master_data',
                                   charset='utf8',
                                   cursorclass=pymysql.cursors.DictCursor)
-    if list(dataset.shape)[1] == 20:
+    if list(data_set.shape)[1] == 35:
         try:
+            print("PC流量导入")
             with connection.cursor () as cursor:
-                sql_rd = 'INSERT INTO `selfhelp_product_data`(`统计日期`, `t_PC端支付金额`, `t_PC端支付买家数`, `t_无线端支付金额`,'\
-                        '`t_无线端支付买家数`,`t_下单转化率`, `t_支付转化率`, `t_支付金额`, `商品名称`, `o_加购人数`, `o_加购商品件数`,'\
-                        '`o_PC端加购商品件数`, `o_商品收藏人数`, `o_无线端加购商品件数`, `f_访客数`, `f_浏览量`, `f_PC端访客数`,'\
-                        '`f_无线端访客数`, `商品ID`, `添加时间`) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,\
-                          %s, %s, %s, %s, %s)'
-                for i in range (len (dataset)):
-                    valuetuple = tuple (list (dataset.values[i,]))
-                    cursor.execute (sql_rd, valuetuple)
-            connection.commit ()
+                sql_rd = 'INSERT INTO `traffic_shop_data`(来源, 来源明细, `访客数`, `访客数变化`,' \
+                         '`新访客`,`新访客变化`, `浏览量`, `浏览量变化`, `人均浏览量`, `人均浏览量变化`, `收藏人数`,' \
+                         '`收藏人数变化`, `加购人数`, `加购人数变化`, `跳失率`, `跳失率变化`, `下单金额`,' \
+                         '`下单金额变化`, `下单买家数`, `下单买家数变化`, `下单转化率`, `下单转化率变化`,' \
+                         '`支付金额`, `支付金额变化`, `支付买家数`, `支付买家数变化`, `支付转化率`, `支付转化率变化`,' \
+                         '`客单价`, `客单价变化`,`UV价值`,`uv价值变化`,`流量来源`,`流量日期`,`添加日期`) ' \
+                         'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,' \
+                         '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                for i in range(len(data_set)):
+                    value_tuple = tuple(list(data_set.values[i,]))
+                    cursor.execute(sql_rd, value_tuple)
+            connection.commit()
 
             with connection.cursor () as cursor:
-                # Read a single record
-                sql = "SELECT max(`统计日期`) as 最晚时间 FROM `selfhelp_product_data`"
-                cursor.execute (sql)
-                result = cursor.fetchone ()
-                print (result)
+                # 统计倒入数据情况
+                sql = "SELECT max(`流量日期`) as 最晚时间 FROM `traffic_shop_data`"
+                cursor.execute(sql)
+                result = cursor.fetchone()
+                print(result)
         finally:
-            connection.close ()
+            connection.close()
     else:
         try:
+            print ("无线流量导入")
             with connection.cursor () as cursor:
-                    sql_rd = 'INSERT INTO `selfhelp_shop_data`(`统计日期`,`t_客单价`,`t_PC端客单价`,`t_无线端客单价`,' \
-                             '`t_老买家数`,`t_新买家数`,`t_下单买家数`,`t_下单商品件数`,`t_支付买家数`,`t_支付商品件数`,' \
-                             '`t_下单且支付金额`,`o_店铺收藏次数`,`o_店铺收藏人数`,`o_商品收藏次数`,`o_商品收藏人数`,`s_售中申请退款金额`,' \
-                             '`s_售中申请退款买家数`,`f_访客数`,`f_老访客数`,`f_PC端访客数`,`f_无线端访客数`,`店铺名称`,`添加时间`) ' \
-                             'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-                    for i in range (len (dataset)):
-                        valuetuple = tuple (list (dataset.values[i, ]))
+                    sql_rd = 'INSERT INTO `traffic_wap_data`(`来源`, `来源明细`,`访客数`,`访客数变化`,`下单金额`,' \
+                             '`下单金额变化`,`下单买家数`,`下单买家数变化`, `下单转化率`,`下单转化率变化`,`支付金额`,' \
+                             '`支付金额变化`,`支付买家数`,`支付买家数变化`,`支付转化率`,`支付转化率变化`,`客单价`,`客单价变化`,' \
+                             '`UV价值`,`uv价值变化`,`流量来源`,`流量日期`,`添加日期`)VALUES(%s, %s, %s, %s, %s, %s, ' \
+                             '%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                    for i in range (len (data_set)):
+                        valuetuple = tuple (list (data_set.values[i, ]))
                         cursor.execute (sql_rd, valuetuple)
             connection.commit()
 
             with connection.cursor() as cursor:
                 # Read a single record
-                sql = "SELECT max(`统计日期`) as 最晚时间 FROM `selfhelp_shop_data`"
+                sql = "SELECT max(`流量日期`) as 最晚时间 FROM `traffic_wap_data`"
                 cursor.execute (sql)
                 result = cursor.fetchone ()
                 print (result)
@@ -139,24 +147,12 @@ def export_mysql(dataset):
     return print("成功插入")
 
 
-# 获得转化后数据
-def get_std_data(target_dir,flow_name):
-    temp_dir = target_dir + flow_name+"_转化"
-    target_dir_list = os.listdir (temp_dir)
-    os.chdir (temp_dir)
-    try:
-        temp_path = os.getcwd ()
-        if temp_path == temp_dir:
-            print ("The work file path was change!")
-    except:
-        print ("The work file path haven't change!")
-    # 获得目标文件夹中的目标文件
-    for target_name in target_dir_list:
-         table = xlrd.open_workbook(target_name)
-         sheet = table.sheet_by_index(0)
-
-         export_mysql(dateset)
-    return print("数据导入完毕")
+# 获得格式转化后数据，并将原始数据中的千分位数据进行替换
+def get_std_data(date_set):
+    data_temp = pd.DataFrame(date_set)
+    for col_name in data_temp:
+        data_temp[col_name] = data_temp[col_name].str.replace (',', '')
+    return data_temp
 
 
 # 删除目标文件中的全部文档
@@ -184,7 +180,7 @@ if __name__ == '__main__':
     # 将流量数据来源进行转化
     for filename in file_name:
         file_list = get_flow_file(sourcedir, filename)
-        tranfer_file(file_list, filename)
+        inport_data(file_list, filename)
     # # 删除转化后标准流量来源数据
     # for filename in file_name:
     #     delete_all (sourcedir, filename)
